d2ef191ec28ec30e44034afd04b74909271da99d
dkumar24 <dkumar24@bechtel.com>
2023-02-06 15:00:24 +0530

    handlebars helper

--- handlerbardemo/index.js
+++ handlerbardemo/index.js
@@ -1,40 +1,53 @@
 import Handlebars from "handlebars";
 
 
-Handlebars.registerHelper("convert",function(e){
-    let newArray=[];
-    e.forEach(element => {
-        let newObject = {name:element.name}
-        newArray.push(newObject);
-    });
-    return newArray;
+
+//HandleBars with Single Expression
+const template = Handlebars.compile(
+    "{{firstName}}"
+)
+console.log(template(({firstName:"Deepak"})));
+//HandleBars with Double Expression
+const template2 = Handlebars.compile(
+    "{{firstName}}  {{lastName}}"
+)
+console.log(template2(({firstName:"Deepak",lastName:"Kumar"})));
+
+//HandleBars with Nested Object
+
+//Without With
+const template3 = Handlebars.compile("{{person.firstName}}  {{person.lastName}}")
+
+console.log(template3({person:{firstName:"Pardeep",lastName:"Kumar"}}))
+
+const template4 = Handlebars.compile("{{#with person}}{{firstName}}  {{lastName}}{{/with}}")
+
+console.log(template4({person:{firstName:"Navdeep",lastName:"Singh"}}))
+
+//Each helper 
+const template5 = Handlebars.compile("{{#each persons}}{{this.name}}{{/each}}")
+console.log(template5({persons:[{name:"Deepak",age:35},{name:"Pardeep",age:39},{name:"Navdeep",age:34},{name:"Sundeep",age:35},{name:"Neetu",age:35}]}))
+
+
+//Custom Helpers
+Handlebars.registerHelper("printPerson",function(person){
+
+    return `${person.name} ${person.age}`;
+});
+
+const template6 = Handlebars.compile("{{#each persons}}{{printPerson this}}{{/each}}")
+console.log(template6({persons:[{name:"Deepak",age:35},{name:"Pardeep",age:39},{name:"Navdeep",age:34},{name:"Sundeep",age:35},{name:"Neetu",age:35}]}));
+
+Handlebars.registerHelper("toUpperCase",function(value){
+    return value.toUpperCase();
 })
 
-
-
-let people = [{
-    name:"valve",
-    val:"val10"
-    },
-    {
-        name:"valve",
-        val:"val10"
-    },
-    {
-        name:"hind",
-        val:"val10"
-    },
-    {
-         name:"test",
-         val:"val10"
-    }];
-
-const template = Handlebars.compile("{{firstName}}{{#each firstName}}{{name:this.name}}{{/each}}");
+const template7 = Handlebars.compile("{{toUpperCase myName}}");
+console.log(template7({myName:"Deepak"}));
 
 
 
-const value =template({firstName:people,lastName:"Kumar"});
-console.log(value);
+
 
    
 
